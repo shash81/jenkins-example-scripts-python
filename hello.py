1b3deab0d0eb30eec7d4c5df692b9418a002cee8
shash81 <37305246+shash81@users.noreply.github.com>
2024-03-17 01:38:55 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
 
 import psutil
-# gives a single float value
-print('The CPU usage is: ', psutil.cpu_percent(1))
-
-# gives an object with many fields
-psutil.virtual_memory()
-# you can convert that object to a dictionary 
-dict(psutil.virtual_memory()._asdict())
-# you can have the percentage of used RAM
-psutil.virtual_memory().percent
-#79.2
-# you can calculate percentage of available memory
-print('available memory', psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
+count = 0
+while (count < 100):
+  count=count+1  
+  # gives a single float value
+  print('The CPU usage is: ', psutil.cpu_percent(1))
+  
+  # gives an object with many fields
+  psutil.virtual_memory()
+  # you can convert that object to a dictionary 
+  dict(psutil.virtual_memory()._asdict())
+  # you can have the percentage of used RAM
+  psutil.virtual_memory().percent
+  #79.2
+  # you can calculate percentage of available memory
+  print('available memory', psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
