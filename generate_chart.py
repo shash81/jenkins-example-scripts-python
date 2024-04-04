1643dedb9b5277c70ad3544de348877cf87fcd47
shash81 <37305246+shash81@users.noreply.github.com>
2024-04-04 22:33:39 +0530

    Create generate_chart.py

---
+++ generate_chart.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Plotting the data
+plt.plot(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Sample Chart')
+plt.grid(True)
+
+# Save the chart as an image
+plt.savefig('sample_chart.png')
+
+# Show the chart
+plt.show()
