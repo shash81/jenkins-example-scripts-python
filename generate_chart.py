7a8f4b2bdb930d4ad9be4374639a3cbb1feb48d2
shash81 <37305246+shash81@users.noreply.github.com>
2024-04-12 23:12:09 +0530

    Update generate_chart.py

--- generate_chart.py
+++ generate_chart.py
@@ -1,18 +1,47 @@
 import matplotlib.pyplot as plt
+import psutil
 
 # Sample data
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 5, 7, 11]
+#x = [1, 2, 3, 4, 5]
+#y = [2, 3, 5, 7, 11]
 
 # Plotting the data
+#plt.plot(x, y)
+#plt.xlabel('X-axis')
+#plt.ylabel('Y-axis')
+#plt.title('Sample Chart')
+#plt.grid(True)
+
+# Save the chart as an image
+#plt.savefig('sample_chart.png')
+
+# Show the chart
+# plt.show()
+
+x=[0]
+y= [0]
+count = 0
+while (count < 20):
+  count=count+1  
+  # gives a single float value
+  x.append(psutil.cpu_percent(1))
+ # print('The CPU usage is: ', psutil.cpu_percent(1))
+  
+  # gives an object with many fields
+  psutil.virtual_memory()
+  # you can convert that object to a dictionary 
+  dict(psutil.virtual_memory()._asdict())
+  # you can have the percentage of used RAM
+  psutil.virtual_memory().percent
+  #79.2
+  y.append(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
+  # you can calculate percentage of available memory
+ # print('available memory', psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
+
 plt.plot(x, y)
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Sample Chart')
 plt.grid(True)
-
-# Save the chart as an image
 plt.savefig('sample_chart.png')
 
-# Show the chart
-# plt.show()
